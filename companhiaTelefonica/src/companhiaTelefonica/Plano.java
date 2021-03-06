4870a7df4de4fe82d6765ec569c6cd9d25260fbb
Bruno Ribeiro <brunorchaves1@gmail.com>
2021-03-06 13:20:36 -0300

    Construtor do plano

--- companhiaTelefonica/src/companhiaTelefonica/Plano.java
+++ companhiaTelefonica/src/companhiaTelefonica/Plano.java
@@ -1,9 +1,20 @@
 package companhiaTelefonica;
+//código, descrição, valor mensal, franquia (minutos) e valor da chamada.
+public class Plano 
+{
+	private int codigo;
+	private String descricao;
+	private int valorMensal;
+	private int franquia;
+	private int valorChamada;
 
-public class Plano {
-
-	public Plano() {
-		// TODO Auto-generated constructor stub
+	public Plano(int codigo ,String descricao, int valorMensal,int franquia,int valorChamada)
+	{
+		this.codigo=codigo;
+		this.descricao  = descricao;
+		this.valorMensal = valorMensal;
+		this.franquia = franquia;
+		this.valorChamada = valorChamada;
 	}
 
 }
