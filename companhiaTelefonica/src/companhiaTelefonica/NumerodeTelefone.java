7db61dd435798e38f4ae93040d3bfb3b326177fc
Bruno Ribeiro <brunorchaves1@gmail.com>
2021-03-17 20:12:36 -0300

    Update NumerodeTelefone.java

--- companhiaTelefonica/src/companhiaTelefonica/NumerodeTelefone.java
+++ companhiaTelefonica/src/companhiaTelefonica/NumerodeTelefone.java
@@ -1,18 +1,63 @@
 package companhiaTelefonica;
+import Plano;
 
 public class NumerodeTelefone extends companhiaEletronica{
 	private int numero;
 	private String cliente;
-	private int plano;
+	private Plano plano;
 	private int dataCancelamento ;
 	private int diaPagamento ;
 
-	public NumerodeTelefone() {
-		this.numero=numero;
-		this.cliente  = cliente;
+	public NumerodeTelefone(int numero, String cliente, Plano plano, int dataCancelamento, int diaPagamento) {
+		super();
+		this.numero = numero;
+		this.cliente = cliente;
 		this.plano = plano;
 		this.dataCancelamento = dataCancelamento;
 		this.diaPagamento = diaPagamento;
 	}
+
+
+	public String getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public Plano getPlano() {
+		return plano;
+	}
+
+
+	public void setPlano(Plano plano) {
+		this.plano = plano;
+	}
+
+
+	public int getDataCancelamento() {
+		return dataCancelamento;
+	}
+
+
+	public void setDataCancelamento(int dataCancelamento) {
+		this.dataCancelamento = dataCancelamento;
+	}
+
+	public int getDiaPagamento() {
+		return diaPagamento;
+	}
+
+	public void setDiaPagamento(int diaPagamento) {
+		this.diaPagamento = diaPagamento;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
 
 }
